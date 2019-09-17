3a2177e1b100ba9ead2e977883df701f3d1d22c1
Yangkang Ou <42730050+oy6033@users.noreply.github.com>
2019-09-17 00:58:32 -0700

    add lib
    Signed-off-by: Yangkang Ou <42730050+oy6033@users.noreply.github.com>

--- src/GUI_Winows.py
+++ src/GUI_Winows.py
@@ -16,7 +16,7 @@ import threading
 import ctypes
 # Leap Instantiation
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-arch_dir = '../lib/x64' if sys.maxsize > 2 ** 32 else '../lib/x86'
+arch_dir = '../lib_Windows/x64' if sys.maxsize > 2 ** 32 else '../lib_Windows/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 
--- src/sensor_leap.py
+++ src/sensor_leap.py
@@ -3,7 +3,7 @@
 
 import os, sys, inspect
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+arch_dir = '../lib_Windows/x64' if sys.maxsize > 2**32 else '../lib_Windows/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 print src_dir
 print arch_dir
