7015649b57d0bcc2d1669b807f2b178db3254d7e
Yangkang Ou <459616756@qq.com>
2019-09-17 12:02:53 -0700

    add Mac support
    Signed-off-by: Yangkang Ou <459616756@qq.com>

--- src/GUI.py
+++ src/GUI.py
@@ -18,8 +18,12 @@ import ctypes
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 if 'linux' in str(sys.platform):
     arch_dir = '../lib_Linux/x64' if sys.maxsize > 2 ** 32 else '../lib_Linux/x86'
-else:
+elif 'win32' in str(sys.platform):
     arch_dir = '../lib_Windows/x64' if sys.maxsize > 2 ** 32 else '../lib_Windows/x86'
+elif 'darwin' in str(sys.platform):
+    arch_dir = '../lib_Mac' if sys.maxsize > 2 ** 32 else '../lib_Mac'
+else:
+    sys.exit()
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 
