e5c953fbbb46be322536d218b96cdd4986e44960
michael <459616756@qq.com>
2019-10-15 16:54:35 -0700

    fix glove issues

--- src/Camera.py
+++ src/Camera.py
@@ -9,7 +9,7 @@ src_dir, arch_dir = check.system_checking()
 class Camera(threading.Thread):
     def __init__(self, fn, maxtimes, l, t, file, message):
         threading.Thread.__init__(self)
-        self.fn = fn + check.video_encoding
+        self.fn = fn
         self.maxtimes = maxtimes
         self.stop = 0
         self.l = l
@@ -49,11 +49,11 @@ class Camera(threading.Thread):
         cv2.destroyAllWindows()
 
         time = str(datetime.datetime.now())[:-7]
-        if (self.file.exists(self.fn)):
-            self.file.delete(self.fn)
-            self.file.insert('', 0, text=self.fn, iid=self.fn, values=(str(self.maxtimes), time))
+        if (self.file.exists(self.fn + check.video_encoding)):
+            self.file.delete(self.fn + check.video_encoding)
+            self.file.insert('', 0, text=self.fn + check.video_encoding, iid=self.fn + check.video_encoding, values=(str(self.maxtimes), time))
         else:
-            self.file.insert('', 0, text=self.fn, iid=self.fn, values=(str(self.maxtimes), time))
+            self.file.insert('', 0, text=self.fn + check.video_encoding, iid=self.fn + check.video_encoding, values=(str(self.maxtimes), time))
 
-        message = self.fn + " has been saved successfully\n"
+        message = self.fn + check.video_encoding + " has been saved successfully\n"
         self.message('videosave', message, 0, len(message), 'purple', False, True)
